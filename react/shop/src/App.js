5fb6b8ba0b687fb2e54039b61103f396924a1575
서지영 Jiyeong Seo <bestzenio@gmail.com>
2023-01-27 23:15:27 +0900

    react 쇼핑몰 만들기 실습 2/3

--- react/shop/src/App.js
+++ react/shop/src/App.js
@@ -1,15 +1,65 @@
 import './App.css';
-import { useState } from 'react'
-import { Nav, Navbar, Container } from 'react-bootstrap'
-import { Routes, Route, useNavigate, Outlet } from "react-router-dom";
+import { createContext, useState } from 'react'
+import { Nav, Navbar, Container, Button } from 'react-bootstrap'
+import { Routes, Route, useNavigate } from "react-router-dom";
+import axios from 'axios'
 import data from './data.js'
 import Detail from './routes/Detail.js'
-import Main from './routes/Main.js'
 
 function App() {
-  let [shoes] = useState(data)
   const navigate = useNavigate();
+  const [shoes, setShoes] = useState(data)  // 상품 리스트
+  const [showBtn, setShowBtn] = useState(true)  // 더 보기 버튼 숨기기
+  const [inventory, setInventory] = useState([10, 11, 12])
 
+  // ^ 메인 UI
+  function Main({shoes}) {
+    // 상품 더 보기 GET
+    function getdata() {
+      Promise
+        .all(['https://codingapple1.github.io/shop/data2.json', 'https://codingapple1.github.io/shop/data3.json']
+        .map((u) => axios.get(u)))
+        .then(
+          axios.spread((res1, res2) => {
+              let copy = [...shoes, ...res1.data]
+              if (shoes.length == 3) {
+                setShoes(copy)
+              } else if (shoes.length > 3) {
+                let copy = [...shoes, ...res2.data]
+                setShoes(copy)
+                setShowBtn(false)
+              }
+          })
+        )
+        .catch((err) => { console.log(err) })
+    }
+
+    return (
+      <>
+        <div className="main-bg"></div>
+        <div className="container">
+          <div className="row">
+            { shoes.map((s,i) => {
+              return (
+                <div
+                  key={i}
+                  className="col-md-4"
+                  onClick={() => navigate(`/detail/${+i}`)}
+                >
+                  <img src={`https://codingapple1.github.io/shop/shoes${i+1}.jpg`} width="80%" alt=""/>
+                  <h4>{s.title}</h4>
+                  <p>{s.price}</p>
+                </div>
+              )})
+            }
+          </div>
+        </div>
+        {showBtn ? <Button variant="outline-secondary" onClick={getdata}>더 보기</Button> : null}
+      </>
+    )
+  }
+
+  // ^ App
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
@@ -24,27 +74,14 @@ function App() {
       </Navbar>
 
       <Routes>
-        <Route path="/" element={<Main shoes={shoes}/>} />
+        <Route path="/" element={<Main shoes={shoes} />} />
         <Route path="*" element={<p>404</p>} />
         <Route path="/detail/:id" element={<Detail shoes={shoes}/>} />
-        {/* <Route path="/event" element={<Event/>} >
-          <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>} />
-          <Route path="two" element={<p>생일쿠폰 받기</p>} />
-        </Route> */}
       </Routes>
 
       
     </div>
   );
 }
-
-// function Event () {
-//   return(
-//     <>
-//       <h4>오늘의 이벤트</h4>
-//       <Outlet></Outlet>
-//     </>
-//   )
-// }
 
 export default App;
--- react/shop/src/routes/Detail.js
+++ react/shop/src/routes/Detail.js
@@ -1,50 +1,102 @@
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { Nav } from 'react-bootstrap'
 
 const Detail = ({shoes}) => {
-  // input
-  const [num, setNum] = useState('')
-  useEffect(() => {
-    if (isNaN(num) === true) {
-      alert('그러지마세요🙄')
-      console.log('string')
-    }
-  }, [num])
-  // url 파라미터 id와 상품 고유 id가 같은 지 체크
+  // Param check : url 파라미터 id와 상품 고유 id가 같은 지 체크
   const {id} = useParams()
-  const shoe = shoes.find((x) => x.id === id)
-  // 2초 뒤 사라지는 알럿
+  const shoe = shoes.find((x) => x.id == id)
+
+  // alert : 5초 뒤 사라지는 알럿
   let [saleAlert, setSaleAlert] = useState(true)
   useEffect(() => {
-    let timesale = setTimeout(() => setSaleAlert(false), 2000)
+    let timesale = setTimeout(() => setSaleAlert(false), 5000)
     return () => {
       clearTimeout(timesale)
     }
   }, [])
 
+  // input : 숫자 외 입력 방지
+  const [num, setNum] = useState('')
+  useEffect(() => {
+    if (isNaN(num) === true) {
+      alert('그러지마세요🙄')
+      setNum('')
+    }
+  }, [num])
+
+  // tab
+  const [tab, setTab] = useState(0)
+
+  // ^ tab contents
+  function TabContent({tab}) {
+    const [fade, setFade] = useState("")
+    useEffect(() => {
+      setFade('end')
+      return () => {
+        setFade('') // clean up
+      }
+    }, [tab])
+
+    return (
+      <div className={`start ${fade}`}>
+        {[
+          <div>내용0</div>,
+          <div>내용1</div>,
+          <div>내용2</div>
+        ][tab]}
+      </div>
+    )
+  }
+
+  // Detail component fade 효과
+  const [fade, setFade] = useState("")
+    useEffect(() => {
+      setFade('end')
+      return () => {
+        setFade('') // clean up
+      }
+    }, [tab])
+
   return (
-    <div className="container">
-      {/* 2초 뒤 사라지는 알럿 */}
+    <div className={`container start ${fade}`}>
       {
         saleAlert === true ?
           <div className="alert alert-warning">
-            2초 이내 구매시 할인!
+            5초 이내 구매시 할인! 🤩
           </div> :
           null
       }
       <div className="row">
         <div className="col-md-6">
-          <img src={process.env.PUBLIC_URL + `/img/shoes${shoe.id+1}.jpg`} width="100%" />
+          <img src={`https://codingapple1.github.io/shop/shoes${shoe.id+1}.jpg`} width="100%" />
         </div>
         <div className="col-md-6">
           <h4 className="pt-5">{shoe.title}</h4>
           <p>{shoe.content}</p>
           <p>{shoe.price}</p>
-          <input className="input" value={num || ""} onChange={(e) => setNum(e.target.value)}/><br/>
-
+          <input 
+            className="input"
+            value={num || ""}
+            onChange={(e) => setNum(e.target.value)}
+          /><br/>
           <button className="btn btn-danger">주문하기</button> 
         </div>
       </div>
+
+      <Nav variant="tabs" defaultActiveKey="link0">
+      <Nav.Item>
+        <Nav.Link eventKey="link0" onClick={() => setTab(0)}>버튼0</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link1" onClick={() => setTab(1)}>버튼1</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link2" onClick={() => setTab(2)}>버튼2</Nav.Link>
+      </Nav.Item>
+    </Nav>
+
+    <TabContent tab={tab}/>
     </div> 
   )
 }
